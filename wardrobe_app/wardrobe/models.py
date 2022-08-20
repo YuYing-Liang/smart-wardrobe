414dc2a1d7a564f0d952a55a1da71a7ab864412e
YuYing Liang <yuying.liang00@gmail.com>
2022-08-20 12:57:10 -0400

    added clothing model

--- wardrobe_app/wardrobe/models.py
+++ wardrobe_app/wardrobe/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+class ClothingItem(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    type = models.CharField(max_length=255, null=False)
+    color = models.CharField(max_length=255, null=False)
+    season = models.CharField(max_length=255, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+
+    def __str__(self):
+        return self.name
